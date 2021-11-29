be9ef3f78960d55da36b7fcbc1c18b830eab4b1d
Derek Reed <derekjreed@users.noreply.github.com>
2021-11-29 23:16:20 +0000

    Add DATABASE_URL for heroku deploy

--- models.py
+++ models.py
@@ -9,8 +9,9 @@ DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'password123')
 DB_NAME = os.getenv('DB_NAME', 'agency')
-DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+#DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
+#    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_PATH = os.getenv('DATABASE_URL')
 
 
 db = SQLAlchemy()
