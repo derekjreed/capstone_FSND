e4aa14f1147afab7f802a09f13661f105f6b5307
Derek Reed <derekjreed@users.noreply.github.com>
2021-11-30 14:40:52 +0000

    Updated comments and tidied up code

--- manage.py
+++ manage.py
@@ -1,14 +1,16 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from app import create_app, APP
+from app import APP
 from models import db
-
+# Script can be used in Heroku/Linux envs for db model
+# creation
 # Changed to uppercase APP as in app.py
 # use:
 # python manage.py db init
 # python manage.py db migrate
 # python manage.py db upgrade
+
 migrate = Migrate(APP, db)
 manager = Manager(APP)
 
